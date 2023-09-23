90268c1b71b5718a03e2f3dc5f91f8f319bc430e
Nicolas Villamarin <villamarin.nico@gmail>
2023-09-23 17:51:47 -0300

    Agregando el proyecto ya terminado

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../assets/pngwing.com.png'
+const Header = () => {
+    return (
+        <>
+        <img src={Logo} alt="" className='Logo'/>  
+        </>
+    )
+}
+
+export default Header
